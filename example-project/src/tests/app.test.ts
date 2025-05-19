9678ff1d493e395a09a18fecbbe40b05532d12dd
Castrozan <castro.lucas290@gmail.com>
2025-05-19 20:52:35 -0300

    remove example project

--- example-project/src/tests/app.test.ts
+++
@@ -1,9 +0,0 @@
-import request from 'supertest';
-import app from '../server.js';
-
-describe('Test app.ts', () => {
-    test('Alive route', async () => {
-        const res = await request(app).get('/api/alive');
-        expect(res.text).toEqual('yes');
-    });
-});
